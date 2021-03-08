1d43242569078acd270d7fbfa22a6aeaf3ba6f7e
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-07 23:57:24 -0500

    feat(server.js): test routes created

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ const app = express()
 
 const { config } = require('./config/index')
 
+app.get('/', (req, res) => {
+  res.send('API working')
+})
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(config.port, () => {
   console.log(`Server listening on port: ${config.port}`)
 })
