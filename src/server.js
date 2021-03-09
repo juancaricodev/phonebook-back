a9bb2a23cce4ffd20ff25c99e68ef0624f79a291
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-09 12:52:34 -0500

    feat[3.4]: delete person method implemented

--- src/server.js
+++ src/server.js
@@ -65,6 +65,13 @@ app.get('/api/persons/:id', (req, res) => {
     })
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(n => n.id !== id)
+
+  res.status(204).end()
+})
+
 app.listen(config.port, () => {
   console.log(`Server listening on port: ${config.port}`)
 })
