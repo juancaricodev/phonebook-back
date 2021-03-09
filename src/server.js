329dcbb4525b62acecb3772aa097b067f6db5b3f
Juan Camilo Rico <juancarico92@gmail.com>
2021-03-09 12:05:35 -0500

    feat: mocked data added

--- src/server.js
+++ src/server.js
@@ -3,6 +3,45 @@ const app = express()
 
 const { config } = require('./config/index')
 
+let persons = [
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1,
+    deleted: false
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2,
+    deleted: false
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3,
+    deleted: false
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4,
+    deleted: false
+  },
+  {
+    id: 5,
+    name: 'Camilo',
+    number: '12345',
+    deleted: false
+  },
+  {
+    id: 6,
+    name: 'Carlos',
+    number: '2345423',
+    deleted: true
+  }
+]
+
 app.get('/', (req, res) => {
   res.send('API working')
 })
