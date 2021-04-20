9a69d064b679a8caba8a26c66a988b32e5195a1c
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-19 19:59:11 -0500

    feat[3.7](server.js): morgan middleware implemented with 'tiny' configuration

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-
+app.use(morgan('tiny'))
 app.use(cors())
 
 const { config } = require('./config/index')
