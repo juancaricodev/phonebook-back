03d2b1372a0ee757d4a92a33f40701a1253afe0d
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-25 10:19:47 -0500

    feat(mongo.js): logic implemented for creating person and get persons recorded

---
+++ src/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const personName = process.argv[3]
+const personNumber = process.argv[4]
+
+const url = `mongodb+srv://admin:${password}@phonebookcluster.lcxfe.mongodb.net/people?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: personName,
+  number: personNumber
+})
+
+if (personName && personNumber) {
+  person.save().then(result => {
+    console.log(`Added ${result.name} number ${result.number} to phone-book`)
+    mongoose.connection.close()
+  })
+} else {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+}
